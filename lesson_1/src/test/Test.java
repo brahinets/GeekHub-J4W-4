f9a4903b11f4b65feb27e4ace57492cf3f6e21c7
YBrahinets <ysb.kanivtsi@gmail.com>
2013-10-11 11:15:50 +0300

    Commit test

---
+++ lesson_1/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yarik Brahinets
+ * Date: 10.10.13
+ * Time: 22:12
+ */
+public class Test {
+
+    public static void main(String args[]){
+         System.out.print("Hello world!!!\n");
+
+    }
+}
