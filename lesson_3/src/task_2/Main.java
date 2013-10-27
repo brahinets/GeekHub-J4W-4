6c3a6059a8cce74de854a4371521ac93350efb3e
YBrahinets <ysb.kanivtsi@gmail.com>
2013-10-27 21:29:26 +0200

    1. Added getters and setters to User class 2. Changed users "database" in Main

--- lesson_3/src/task_2/Main.java
+++ lesson_3/src/task_2/Main.java
@@ -20,37 +20,36 @@ public class Main {
     * */
     public static void main(String args[])  {
         List<User> users = new ArrayList<>();
-        users.add(0, new User("login1","pass1"));
-        users.add(1, new User("login1","pass1"));
+        users.add(0, new User("login0","pass0"));
         users.add(1, new User("login1","pass1"));
         users.add(2, new User("login2","pass2"));
         users.add(3, new User("login3","pass3"));
         users.add(4, new User("login4","pass4"));
         users.add(5, new User("login5","pass5"));
 
-        User user = new User("login1","pass1");
-        User user1 = new User("login1","pass");
-        User user2 = new User("","");
+        User userTest = new User("login1","pass1");
+        User userTest1 = new User("login1","pass");
+        User userTest2 = new User("","");
 
-        System.out.println("\nTry to login user with login: '"+user.login+"', and pass '"+user.password+"'");
+        System.out.println("\nTry to login user with login: '"+userTest.getLogin()+"', and pass '"+userTest.getPassword()+"'");
         try{
-            user.auth(users);
-            System.out.println("User '"+user.login+"' successfully logined");
+            userTest.auth(users);
+            System.out.println("User '"+userTest.getLogin()+"' successfully logined");
         } catch (AuthException e) {
             System.out.println("Caught " + e);
         }
-        System.out.println("\nTry to login user with login: '"+user1.login+"', and pass '"+user1.password+"'");
+        System.out.println("\nTry to login user with login: '"+userTest1.getLogin()+"', and pass '"+userTest1.getPassword()+"'");
         try{
-            user1.auth(users);
-            System.out.println("User '"+user1.login+"' successfully logined");
+            userTest1.auth(users);
+            System.out.println("User '"+userTest1.getLogin()+"' successfully logined");
         } catch (AuthException e) {
             System.out.println("Caught " + e);
         }
 
-        System.out.println("\nTry to login user with login: '"+user2.login+"', and pass '"+user2.password+"'");
+        System.out.println("\nTry to login user with login: '"+userTest2.getLogin()+"', and pass '"+userTest2.getPassword()+"'");
         try{
-            user2.auth(users);
-            System.out.println("User '"+user2.login+"' successfully logined");
+            userTest2.auth(users);
+            System.out.println("User '"+userTest2.getLogin()+"' successfully logined");
         } catch (AuthException e) {
             System.out.println("Caught " + e);
         }
--- lesson_3/src/task_2/User.java
+++ lesson_3/src/task_2/User.java
@@ -22,8 +22,24 @@ import java.util.List;
  **/
 public class User {
 
-    String login;
-    String password;
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     User(String login, String password){
         this.login = login;
